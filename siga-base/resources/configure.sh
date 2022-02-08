e24cb70e2205ea0339e3519d065adf1898609ab2
Bruno Casas <brunocasas04@gmail.com>
2022-02-07 20:41:54 -0400

    Add local ckeditor zip

--- siga-base/resources/configure.sh
+++ siga-base/resources/configure.sh
@@ -2,45 +2,12 @@
 
 set -e
 
-BASE_URL="https://191.252.111.124/files/siga/"
-
-RESOURCES=(
-    "ckeditor_4.5.7_full.zip"
-)
-
-SHA_256_SUN=(
-    "323b535003a2b69e5faa306cdb3744cb6879ae402512b0ee8d260a2cb87fc30c"
-)
-
 mkdir -p $SIGA_DIR/logs
 mkdir -p $SIGA_DIR/configs
 mkdir -p $SIGA_DIR/deployments
 
 WORK_DIR="${SIGA_DIR}/tmp"
 cd $WORK_DIR
-
-echo "Downloading resources..."
-for i in "${!RESOURCES[@]}"; do
-    resource=${RESOURCES[$i]}
-    file="$(cut -d' ' -f2 <<<"$resource")"
-
-    attempts=0
-    while [ $attempts -lt 2 ]; do
-        echo "Downloading $file:"
-        curl --connect-timeout 10 -k --progress-bar $BASE_URL$file -o $file 2>&1
-
-        echo -n "Health check for $file: "
-        hash="$(sha256sum $file | cut -d ' ' -f 1)"
-        if [ $hash = ${SHA_256_SUN[$i]} ]; then
-            echo "SUCCESS"
-            attempts=0
-            break
-        fi
-        ((attempts++))
-        echo "FAIL"
-        echo "Trying to download again..."
-    done
-done
 
 echo -n "Performing initial setups on Wildfly. "
 jboss-cli.sh >/dev/null <<EOF
@@ -51,6 +18,8 @@ module add --name=com.mysql --resources=$SIGA_DIR/tmp/mysql-connector-java-8.0.2
 /subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-class-name=com.mysql.cj.jdbc.Driver)
 /subsystem=security/security-domain=sp/:add(cache-type=default)
 /subsystem=security/security-domain=sp/authentication=classic:add(login-modules=[{code=org.picketlink.identity.federation.bindings.jboss.auth.SAML2LoginModule, flag=>required}])
+/subsystem=undertow/server=default-server/http-listener=default/:write-attribute(name=max-post-size,value=117440512)
+/subsystem=undertow/server=default-server/https-listener=https/:write-attribute(name=max-post-size,value=117440512)
 
 run-batch
 stop-embedded-server
@@ -61,6 +30,7 @@ if [ $? -eq 0 ]; then
     echo "SUCCESS"
 else
     echo "FAIL"
+    exit 1
 fi
 
 echo -n "Creating wildfly management user. "
@@ -70,6 +40,7 @@ if [ $? -eq 0 ]; then
     echo "SUCCESS"
 else
     echo "FAIL"
+    exit 1
 fi
 
 echo -n "extracting the ckeditor: "
@@ -79,6 +50,7 @@ if [ $? -eq 0 ]; then
     echo "SUCCESS"
 else
     echo "FAIL"
+    exit 1
 fi
 
 rm -rf $WORK_DIR/*
