c868292113580fc94536f7787e9bf9e83894268e
Bruno Casas <brunocasas04@gmail.com>
2023-11-18 17:57:17 -0400

    Update wildfly to 26.1.3.Final

--- siga-base/resources/configure.sh
+++ siga-base/resources/configure.sh
@@ -10,14 +10,12 @@ WORK_DIR="${SIGA_DIR}/tmp"
 cd $WORK_DIR
 
 echo -n "Performing initial setups on Wildfly. "
-jboss-cli.sh >/dev/null <<EOF
+jboss-cli.sh <<EOF
 embed-server --std-out=echo
 batch
 
-module add --name=com.mysql --resources=$SIGA_DIR/tmp/mysql-connector-java-8.0.24.jar --dependencies=javax.api,javax.transaction.api
+module add --name=com.mysql --resources=$SIGA_DIR/tmp/mysql-connector-j-8.2.0.jar --dependencies=javax.api,javax.transaction.api
 /subsystem=datasources/jdbc-driver=mysql:add(driver-name=mysql,driver-module-name=com.mysql,driver-class-name=com.mysql.cj.jdbc.Driver)
-/subsystem=security/security-domain=sp/:add(cache-type=default)
-/subsystem=security/security-domain=sp/authentication=classic:add(login-modules=[{code=org.picketlink.identity.federation.bindings.jboss.auth.SAML2LoginModule, flag=>required}])
 /subsystem=undertow/server=default-server/http-listener=default/:write-attribute(name=max-post-size,value=117440512)
 /subsystem=undertow/server=default-server/https-listener=https/:write-attribute(name=max-post-size,value=117440512)
 /subsystem=sar/:remove
--- wildfly/install.sh
+++ wildfly/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 FILE="wildfly-$WILDFLY_VERSION.tar.gz"
-URL="https://download.jboss.org/wildfly/$WILDFLY_VERSION/wildfly-$WILDFLY_VERSION.tar.gz"
+URL="https://github.com/wildfly/wildfly/releases/download/$WILDFLY_VERSION/wildfly-$WILDFLY_VERSION.tar.gz"
 
 addgroup -g 1000 jboss
 adduser -u 1000 -S -G jboss -h /opt/jboss -s /bin/ash jboss
@@ -13,7 +13,7 @@ echo "Downloading WildFly..."
 attempts=0
 while [  $attempts -lt 2 ]; do
     echo "Downloading $FILE:"
-    curl --connect-timeout 10 --progress-bar -O $URL 2>&1
+    curl --connect-timeout 10 --progress-bar -L -O $URL 2>&1
 
     echo -n "Health check for $FILE: "
     sha1sum $FILE | grep $WILDFLY_SHA1 > /dev/null
